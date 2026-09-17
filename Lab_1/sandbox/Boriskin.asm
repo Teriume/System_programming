e663b65ab8fdd9520c6577b2ca534daf6301111f
Terium <iliyaboriskin@Makinbuku.local>
2026-09-18 00:28:00 +0300

    Fix Boriskin.asm: valid FASM ELF32 source (no entry directive)

--- Lab_1/sandbox/Boriskin.asm
+++ Lab_1/sandbox/Boriskin.asm
@@ -1,34 +1,19 @@
-format ELF32
+format ELF
+
+section ".data" writeable
+msg db 'Борискин Илья Васильевич', 10
+msg_len = $ - msg
+
+section ".text" executable
+use32
 public _start
-
-section '.data' writeable
-    fam db 'Борискин', 10
-    fam_len = $ - fam
-    name db 'Илья', 10
-    name_len = $ - name
-    pat db 'Васильевич', 10
-    pat_len = $ - pat
-
-section '.text' executable
 _start:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, fam
-    mov edx, fam_len
-    int 0x80
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, msg
+	mov edx, msg_len
+	int 0x80
 
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, name
-    mov edx, name_len
-    int 0x80
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, pat
-    mov edx, pat_len
-    int 0x80
-
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+	mov eax, 1
+	xor ebx, ebx
+	int 0x80
